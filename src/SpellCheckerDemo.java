42c76cbe8dce644123f27f6dea69954f826d9adb
PiccoloMondoC <119958916+PiccoloMondoC@users.noreply.github.com>
2022-12-13 12:19:58 -0600

    Update SpellCheckerDemo.java

--- src/SpellCheckerDemo.java
+++ src/SpellCheckerDemo.java
@@ -2,8 +2,8 @@
  * A java class to define a spell checking class using a hash table.
  * The program will: read a set of words W, from a words.txt file
  * and store them in a hash table; implement a spellCheck function
- * that performs a spell check on a string s, entered by user at the
- * console, using the words in the hash table as the dictionary. Return
+ * that performs a spell check on a string s, from demo.txt file,
+ * using the words in the hash table as the dictionary. Return
  * an iterable collection of correctly spelled words, or if not in the
  * hash table, return a list of words in W that could be a correct
  * spelling of s. The program will also handle all the ways that s might
@@ -17,92 +17,61 @@
  * Assignment Project 2 Part I
  * Instructor Professor Steve Johnson
  * Due Date December 10, 2022
+ * 
+ * 
+ * Data Files
+ * ========== 
+ * dictionary => words.txt
+ * words/sentence to check => demo.txt
+ * 
+ * User can replace these files with new files with the same name.
+ * 
+ * 
+ * Code
+ * ====
+ * SpellChecker.java
+ * SpellCheckerDemo.java
+ * 
  */
+
 
 import java.io.*;
 import java.util.*;
-import java.util.regex.*;
 
-public class SpellCheckerDemo {
-    //private static Object dictionary;
-    private static boolean suggestWord;
 
-    public static void main(String[] args) throws IOException {
-        String dictionaryFileName = "words.txt"; // Dictionary file name 
+// Class declaration
+public class SpellCheckerDemo{
 
-        ChainingSpellChecker.getNumOfItemsInFile(dictionaryFileName);
-        int numWords = ChainingSpellChecker.getNumOfItemsInFile(dictionaryFileName);
-        System.out.println(
-            "Number of words in " + dictionaryFileName + " is " + numWords
-            );
-        int tableSize = numWords;
-        tableSize *= 2;
-        // Check if maxSize isPrime
-        if (!ChainingSpellChecker.isPrime(tableSize)) {
-            tableSize = ChainingSpellChecker.nextPrime( tableSize );
-        }
+// main method
+public static void main(String args[]) {
 
-        Scanner sc = new Scanner(new File(dictionaryFileName));
-		ChainingSpellChecker hashTable = new ChainingSpellChecker(tableSize);
-		
-		while(sc.hasNext()){
-			String s = sc.nextLine();
-			hashTable.insert(s);			
-		}
+    String fileName;
 
-        numWords = hashTable.getNumOfItemsInFile(dictionaryFileName);
+    // Create spellchecker object    
+    SpellChecker demoSpeller = new SpellChecker();
 
-        System.out.println("\nSpell Checker Demo\n\n");
-        System.out.println(
-            "Number of words in " + dictionaryFileName + " is:\t\t" + numWords
-            );
-        System.out.println(
-            "The number of words currently in hash table is:\t" 
-            + hashTable.getSize()
-            );
-        System.out.println(
-            "The Capacity of the hash table is:\t\t" + tableSize
-            );
-        hashTable.printHashTable ();
+    // Instantiatiate the dictionary file
+    demoSpeller.setDictionaryFileName("words.txt");
+    fileName = demoSpeller.getDictionaryFileName();
 
-               
-		Scanner scanWord = new Scanner(System.in);
-        // Prompt the user for text to check the spelling 
-        System.out.print("\nType a sentence to check the spelling: ");
-
-        
-        
-        String stringInput = scanWord.nextLine();
-        
-        System.out.print(
-            "\nYou entered: " +stringInput 
-            +"\n\nConverting to all lowercase and initiating spell check...\n\n"
-            );
-        stringInput = stringInput.toLowerCase();
-        String[] splitInput = stringInput.split(" ");
-        for (String word : splitInput) {
-            if (hashTable.containsKey(word)) {
-                System.out.println(word + " : correct\n");
-            }else{
-                System.out.println(word + " : incorrect\n");
-//                printSuggestions(suggest(word));
-            }
-        }
-	}
-/*
-    private static void printSuggestions(Object object) {
-        Set<String> suggestions = new HashSet<>();
-        Object word;
-        Set<String> alternates = makeAlternates(word);
-        for (String alternate : alternates) {
-            if (hashTable.contains(alternate) {
-                suggestions.add(alternate);
-            }
-        }
-        return suggestions;
-    }*/
-
-    private static Object suggest(String word) {
-        return null;
+    List<String> correctWords = demoSpeller.setIncorrectWords("demo.txt");
+    
+    System.out.println("");
+    // Print correct words
+    for (String eachWord : demoSpeller.getCorrectWords()) {
+        System.out.println("Correct: " + eachWord);
     }
+    System.out.println("");
+
+    List<String> incorrectWords = demoSpeller.setIncorrectWords("demo.txt");
+    
+    for(String eachWord : incorrectWords){
+        Set<String> suggSet = demoSpeller.getSuggestions(eachWord);
+
+        // Print incorrect words and suggestions to replace them
+        System.out.println("Incorrect: " + eachWord);
+        System.out.println("Suggestions: ");
+        System.out.println(suggSet + "\n");
+    }
+}
 }
